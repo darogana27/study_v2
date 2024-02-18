006c703e17fad5c04627eca187fa58e2f42655a9
darogana27 <darogana27@gmail.com>
2024-02-18 18:49:42 +0900

    各種修正

--- api+gateway+lambda/main.tf
+++ api+gateway+lambda/main.tf
@@ -5,8 +5,7 @@ provider "aws" {
 resource "aws_lambda_function" "lambda-test" {
   function_name = "lambda-test"
   role          = "arn:aws:iam::671522354054:role/service-role/lambda-test-role-i43ocw46"
-  #   filename = aws_s3_object.lambda-test
-  s3_bucket = module.s3.s3_id
+  s3_bucket = data.terraform_remote_state.common.outputs.s3_bucket_arn
   s3_key    = "lambda-test.zip"
   handler   = "lambda_function.lambda_handler"
   runtime   = "python3.12"
@@ -19,8 +18,18 @@ resource "aws_apigatewayv2_api" "api-gateway-test" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-state-2024-0211"
+    bucket = "terraform-state-2024-0218"
     key = "study_v2/api_gateway+lambda/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+data "terraform_remote_state" "common" {
+  backend = "s3"
+
+  config = {
+    bucket = "terraform-state-2024-0218"
+    key    = "common/s3/terraform.tfstate"
     region = "ap-northeast-1"
   }
 }
--- api+gateway+lambda/s3.tf
+++
@@ -1,4 +0,0 @@
-module "s3" {
-  source         = "../modules/s3"
-  s3_bucket_name = "lambda-package-20240218"
-}
