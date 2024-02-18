0620bf9c3f64adde8cf18a31f2b5d1817af4b719
darogana27 <darogana27@gmail.com>
2024-02-18 15:08:13 +0900

    s3の削除

--- videos/s3/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  s3_bucket_upload     = "serverless-videos-upload"
-  s3_bucket_transcoded = "serverless-videos-transcoded"
-}
--- videos/s3/main.tf
+++
@@ -1,9 +0,0 @@
-module "s3_bucket_upload" {
-  source         = "../../modules/s3"
-  s3_bucket_name = local.s3_bucket_upload
-}
-
-module "s3_bucket_transcoded" {
-  source         = "../../modules/s3"
-  s3_bucket_name = local.s3_bucket_transcoded
-}
