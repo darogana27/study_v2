7818376d397fad3f815d1804bd27e0172df34f5f
darogana27 <darogana27@gmail.com>
2024-06-24 23:37:31 +0900

    lambdaの作成

--- amount-of-electricity/lambda.tf
+++ amount-of-electricity/lambda.tf
@@ -1,28 +1,15 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
+resource "aws_iam_role" "line_notify_role" {
+  name               = "line_notify_role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_execution_policy" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.lambda_add_policy.arn
+resource "aws_iam_role_policy_attachment" "line_notify_policy" {
+  role       = aws_iam_role.line_notify_role.name
+  policy_arn = aws_iam_policy.line_notify_policy.arn
 }
 
-resource "aws_iam_policy" "lambda_add_policy" {
-  name = "lambda_add_policy"
+resource "aws_iam_policy" "line_notify_policy" {
+  name = "line_notify_policy"
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -49,13 +36,71 @@ resource "aws_iam_policy" "lambda_add_policy" {
   })
 }
 
-resource "aws_lambda_function" "Line_Notify" {
+resource "aws_lambda_function" "line_notify" {
   filename      = "${path.module}/lambda/Line_Notify.zip"
-  function_name = "Line_Notify"
-  role          = aws_iam_role.iam_for_lambda.arn
+  function_name = "line_notify"
+  role          = aws_iam_role.line_notify_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.12"
   timeout       = 5
+
+  ephemeral_storage {
+    size = 1024 # Min 512 MB and the Max 10240 MB
+  }
+}
+
+resource "aws_ecr_repository" "daily-electricity"{
+  name = "daily-electricity"
+}
+
+# variable "lambda_image_uri" {
+#   description = "The URI of the ECR image to use for the Lambda function"
+#   default     = "${local.account_id}.dkr.ecr.${local.env.region}.amazonaws.com/daily-electricity:latest"
+# }
+
+resource "aws_iam_role" "daily-electricity_role" {
+  name               = "daily-electricity_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "daily-electricity_policy" {
+  role       = aws_iam_role.daily-electricity_role.name
+  policy_arn = aws_iam_policy.daily-electricity_policy.arn
+}
+
+resource "aws_iam_policy" "daily-electricity_policy" {
+  name = "daily-electricity_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ssm:GetParameter"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:PutObject"
+        ],
+        Resource = [
+            "arn:aws:s3:::amount-of-electricity/*",
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "daily-electricity" {
+  package_type  = "Image"
+  image_uri = "${local.account_id}.dkr.ecr.${local.env.region}.amazonaws.com/daily-electricity:latest"
+  function_name = "daily-electricity"
+  role          = aws_iam_role.daily-electricity_role.arn
+  memory_size = 512
+  timeout       = 60
 
   ephemeral_storage {
     size = 1024 # Min 512 MB and the Max 10240 MB
--- amount-of-electricity/locals.tf
+++ amount-of-electricity/locals.tf
@@ -3,4 +3,5 @@ locals {
     region = "ap-northeast-1"
   }
   name = "electricity"
+  account_id = data.aws_caller_identity.self.account_id
 }
