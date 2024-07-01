82230ae4a2b1365a025bf61373164fe8438b2f5c
darogana27 <darogana27@gmail.com>
2024-07-01 23:24:53 +0900

    fmt

--- amount-of-electricity/dynamodb.tf
+++ amount-of-electricity/dynamodb.tf
@@ -2,13 +2,13 @@ module "dynamodb" {
   source = "../modules/dynamodb"
   dynamodbs = {
     table1 = {
-      name                = "example1"
-      billing_mode        = "PROVISIONED"
-      hash_key            = "id"
-      range_key           = "rangeKey"
-      read_capacity       = 5
-      write_capacity      = 5
-      hash_key_type       = "S"
+      name           = "example1"
+      billing_mode   = "PROVISIONED"
+      hash_key       = "id"
+      range_key      = "rangeKey"
+      read_capacity  = 5
+      write_capacity = 5
+      hash_key_type  = "S"
       attributes = [
         { name = "id", type = "S" },
         { name = "rangeKey", type = "S" },
@@ -32,11 +32,11 @@ module "dynamodb" {
       }
     },
     table2 = {
-      name                = "example2"
-      billing_mode        = "PAY_PER_REQUEST"
-      hash_key            = "userId"
-      range_key           = "timestamp"
-      hash_key_type       = "S"
+      name          = "example2"
+      billing_mode  = "PAY_PER_REQUEST"
+      hash_key      = "userId"
+      range_key     = "timestamp"
+      hash_key_type = "S"
       attributes = [
         { name = "userId", type = "S" },
         { name = "timestamp", type = "S" },
