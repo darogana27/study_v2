11210b0236c857c27262b3b33fe068c6fb1bb4d0
darogana27 <darogana27@gmail.com>
2025-02-08 21:57:24 +0900

    Update dynamodb.tf

--- product/what-aws-news/dynamodb.tf
+++ product/what-aws-news/dynamodb.tf
@@ -5,14 +5,14 @@ module "dynamodb" {
   dynamodbs = {
     article = {
       billing_mode = "PAY_PER_REQUEST"
-      hash_key     = "URL"
+      hash_key     = "link"
       range_key    = "公開日"
       # read_capacity  = 1
       # write_capacity = 1
       hash_key_type = "S"
       attributes = [
-        { name = "URL", type = "S" }, # パーティションキー属性
-        { name = "公開日", type = "S" }  # ソートキー属性
+        { name = "link", type = "S" }, # パーティションキー属性
+        { name = "公開日", type = "S" }   # ソートキー属性
       ]
       ttl = {
         attribute_name = "TimeToExist"
